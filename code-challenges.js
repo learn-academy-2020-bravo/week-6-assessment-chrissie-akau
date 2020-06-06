4e8183caba062d7fe8bf4b8ac2c13e606298919a
chrissie-akau <cakau9@gmail.com>
2020-06-05 18:51:48 -0700

    All Done

--- code-challenges.js
+++ code-challenges.js
@@ -8,25 +8,56 @@ var people = [
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
   { name: "arthur dent", occupation: "radio employee" }
 ]
-// Expected output example: "Ford Prefect is a hitchhiker." "Zaphod Beeblebrox is a president of the galaxy." "Arthus Dent is a radio employee."
 
+//create a function that takes in an object
+const capitalNames = (object) => {
+  //.map through object and manipulate value by using .split to access an array of the two words in name obejct.  map through this new array of name split into letters
+  return object.map((value) => {
+    value.name = value.name.split(' ').map((word) => {
+      //return the first index uppercased and then combine with the rest of the word still lower case
+      return word[0].toUpperCase() + word.slice(1)
+      //join it back togeher
+    }).join(" ")
+    //final return is a combination of the newly capitalized first and last name in name, in addition to "is a" and the occupation with a period at the end.  Spacing is important to have the correct output
+    return value.name+ ' is a ' + value.occupation + '.'
+  })
+}
 
-
+console.log(capitalNames(people))
+// Expected output example: "Ford Prefect is a hitchhiker." "Zaphod Beeblebrox is a president of the galaxy." "Arthur Dent is a radio employee."
 
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the remainder of the numbers when divided by 3.
 
 var testingArray1 = [23, "Heyyyy!", 45, -9, 0, "Yo", false]
-// Expected output: [ 2, 0, -0, 0 ]
 var testingArray2 = [5, "Hola", 43, -34, "greetings", true]
+
+//create a function that takes in a mixed data array
+const numsOnly = (array) => {
+  //use .filter to go through array and use the typeof method to return an array with only the numbers
+  let filterNums = array.filter(value => typeof value == "number")
+    //.map through filterNums and get the remainder of the numbers when divided by 3 using modulo
+    return filterNums.map(value => value % 3)
+}
+
+console.log(numsOnly(testingArray1))
+// Expected output: [ 2, 0, -0, 0 ]
+console.log(numsOnly(testingArray2))
 // Expected output: [ 2, 1, -1 ]
-
-
-
-
 
 // --------------------3) Create a function that takes in two arrays as arguments returns one array with no duplicate values.
 
 var testingArray3 = [3, 7, "hi", 10, 3, "hello", 4, "hi"]
 var testingArray4 = [7, "hi", 3, 1, "hi", 4, "hello", 4, 7]
+
+//create a function that takes in two arrays 
+const noDupes = (array1, array2) => {
+      //create variable that combines the two arugments into one new array using the spread operator
+      let newArray = [...array1, ...array2]
+      //use .filter to get rid of duplicates in newArray using .indexOf on the array itself
+      let filtArray = newArray.filter((value, index, array) => index === array.indexOf(value))
+          //return variable with the newly filtered array
+          return filtArray
+}
+console.log(noDupes(testingArray3, testingArray4))
 // Expected output: [ 3, 7, "hi", 10, "hello", 4, 1 ]
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -5,24 +5,24 @@
 
 # app/controller/blog_posts_controller.rb
 
-# 1)
+# 1) This is a controller file we created for the model called Blog Posts. This will handle all the methods/logic we want to use and coordinates the interaction between the user, views, and model.
 class BlogPostsController < ApplicationController
   def index
-    # 2)
+    # 2) This is an instance variable called @posts that will show us all the blog posts avaiable in the Blog Posts table.  It will be retrieved using a GET HTTP Verb, and in the url be called by /blogposts.  Part of the READ portion in CRUD.
     @posts = BlogPost.all
   end
 
   def show
-    # 3)
+    # 3) This instance variable is going to use the GET HTTP verb,  and asking to show the particular Blog Post by using it's unique id.  You would retrieve this by putting /blogposts/id# in the url in order to view it. Part of the READ portion in CRUD.
     @post = BlogPost.find(params[:id])
   end
 
-  # 4)
+  # 4) This is a method that is a midway point between CREATE and READ in CRUD and is the Rails convention for the route that displays a form to the user.  Uses the GET HTTP verb.
   def new
   end
 
   def create
-    # 5)
+    # 5)This instance varaible is used to create a new blog post using the strong params method called blog_post_params. This is the CREATE part in the CRUD. It's saying to create the new blog post if it has a title and content, but if the user doesn't do it correctly, to send them back to the form to fill it out again with the correct parameters.  Will use the POST HTTP verb.
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to @post
@@ -36,15 +36,15 @@ class BlogPostsController < ApplicationController
     if @post.delete
       redirect_to blog_posts_path
     else
-      # 6)
+      # 6)This will redirect the browser back to the post using the blog posts' unique id if it isn't deleted properly.
       redirect_to blog_post_path(@post)
     end
   end
 
-  # 7)
+  # 7)This indicates strong params for Blog Post.  It helps add a level of security to the information being provided by ensuring we only get the information we absoultely want into the Blog Post table.
   private
   def blog_post_params
-    # 8)
+    # 8)Any blog post entry MUST HAVE a title and content and will be the only content that will be saved into the Blog Post table.
     params.require(:blog_post).permit(:title, :content)
   end
 
@@ -53,8 +53,8 @@ end
 
 # app/models/blog_post.rb
 
-# 9)
+# 9)This is the Blog Post Application record, this is where we would add any relationships to other tables in it and any validations we might have for Blog Posts.
 class BlogPost < ApplicationRecord
-  # 10)
+  # 10)This establishes that there is a relationship between a table named Comments and Blog Post.  Basically comments can inherit from Blog Post because Comments belongs_to Blog Post, while Blog Post has_man comments.  The foreign key solidfying this relationship would be stored in the Comments table as blog_post_id: integer.
   has_many :comments
 end
